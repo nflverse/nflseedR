bffb62d31f42c8ac87c3e80e69158b6126a29032
mrcaseb <roemerle@gmail.com>
2021-01-20 20:40:33 +0100

    Fix data docs

--- R/data_doc.R
+++ R/data_doc.R
@@ -5,9 +5,9 @@
 #' information, including franchises in multiple cities:
 #' \describe{
 #'   \item{team}{Team abbreviation}
-#'   \item{conf}{Complete Team name}
-#'   \item{division}{Team id used in the roster function}
-#'   \item{sdiv}{Nickname}
+#'   \item{conf}{Conference abbreviation}
+#'   \item{division}{Division name}
+#'   \item{sdiv}{Division abbreviation}
 #' }
 #' This data frame is created using the `teams_colors_logos` data frame of the
 #' `nflfastR` package. Please see `data-raw/divisions.R` for the code to create
