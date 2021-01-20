e2b1ce6ce6c14dd6f803a13a6ed2dd9c823f3c5e
mrcaseb <roemerle@gmail.com>
2021-01-20 18:21:08 +0100

    update package docs

--- R/nflseedR-package.R
+++ R/nflseedR-package.R
@@ -5,5 +5,11 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 #' @importFrom magrittr %>%
+#' @importFrom rlang inform
+#' @importFrom crayon red
+#' @importFrom cli symbol
+#' @import dplyr
+#' @importFrom glue glue
+#' @import gsubfn
 ## usethis namespace: end
 NULL
