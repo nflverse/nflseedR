65287777137e5372812af25b993c4e1f3c883322
mrcaseb <roemerle@gmail.com>
2021-01-26 12:50:51 +0100

    Add simulation function

---
+++ R/sim_helper.R
@@ -0,0 +1,190 @@
+simulate_round <- function(sim_round,
+                           sim_rounds,
+                           sims_per_round,
+                           schedule,
+                           simulations,
+                           weeks_to_sim,
+                           estimate_games,
+                           simulate_games,
+                           tiebreaker_depth,
+                           .debug,
+                           playoff_seeds,
+                           p
+                           ){
+
+  # start us off
+  # report(glue("Beginning simulation round {sim_round} of {sim_rounds}"))
+
+  # iteration sims
+  iter_sims <- sims_per_round * (sim_round - 1) + seq_len(sims_per_round)
+  iter_sims <- iter_sims[iter_sims <= simulations]
+  iter_sims_num <- length(iter_sims)
+
+  # games have copies per sim
+  games <- schedule[rep(seq_len(nrow(schedule)), each = iter_sims_num), ] %>%
+    mutate(sim = rep(iter_sims, nrow(schedule))) %>%
+    select(sim, everything())
+
+  #### SIMULATE REGULAR SEASON ####
+
+  # simulate remaining regular season games
+  for (week_num in weeks_to_sim)
+  {
+    # estimate and simulate games
+    # report(glue("Processing Week {week_num}"))
+    games <- games %>%
+      estimate_games() %>%
+      simulate_games(week_num)
+  }
+
+  #### FIND DIVISIONAL STANDINGS AND PLAYOFF SEEDINGS ####
+
+  standings_and_h2h <- games %>%
+    compute_division_ranks(
+      tiebreaker_depth = tiebreaker_depth,
+      .debug = .debug
+    ) %>%
+    compute_conference_seeds(
+      h2h = .$h2h,
+      tiebreaker_depth = tiebreaker_depth,
+      .debug = .debug,
+      playoff_seeds = playoff_seeds
+    )
+
+  teams <- standings_and_h2h$standings
+  h2h_df <- standings_and_h2h$h2h
+
+
+  #### PLAYOFFS ####
+
+  # week tracker
+  week_num <- games %>%
+    filter(game_type == "REG") %>%
+    pull(week) %>%
+    max()
+
+  week_max <- week_num + 4L
+
+  # identify playoff teams
+  playoff_teams <- teams %>%
+    filter(!is.na(seed)) %>%
+    select(sim, conf, seed, team) %>%
+    arrange(sim, conf, seed)
+
+  # num teams tracker
+  num_teams <- playoff_teams %>%
+    group_by(sim, conf) %>%
+    summarize(count = n()) %>%
+    pull(count) %>%
+    max()
+
+
+  # playoff weeks
+  while (num_teams > 1) {
+
+    # inseed_numement week number
+    week_num <- week_num + 1
+    report(paste("Processing Playoffs Week", week_num))
+
+    # seed_numeate games if they don't already exist
+    if (!any(games$week == week_num)) {
+      # teams playing this round
+      add_teams <- playoff_teams %>%
+        group_by(sim, conf) %>%
+        slice((2^ceiling(log(num_teams, 2)) - num_teams + 1):num_teams) %>%
+        mutate(round_rank = row_number()) %>%
+        ungroup()
+
+      # games to seed_numeate
+      add_games <- add_teams %>%
+        inner_join(add_teams, by = c("sim", "conf")) %>%
+        filter(round_rank.x > round_rank.y) %>%
+        filter(round_rank.x + round_rank.y == max(round_rank.x) + 1) %>%
+        select(-conf, -seed.x, -seed.y, -round_rank.x, -round_rank.y) %>%
+        rename(away_team = team.x, home_team = team.y) %>%
+        mutate(
+          week = week_num,
+          game_type = case_when(
+            week_max - week_num == 3 ~ "WC",
+            week_max - week_num == 2 ~ "DIV",
+            week_max - week_num == 1 ~ "CON",
+            week_max - week_num == 0 ~ "SB",
+            TRUE ~ NA_character_
+          )
+        )
+
+      ####
+      # add your own code here to add additional information to add_games
+      # that is needed for your own versions of estimate_games() and simulate_games()
+      ####
+
+      # add to games
+      games <- bind_rows(games, add_games)
+    }
+
+    # process any new games
+    games <- games %>%
+      estimate_games() %>%
+      simulate_games(week_num)
+
+    # record losers
+    teams <- games %>%
+      filter(week == week_num) %>%
+      double_games() %>%
+      filter(outcome == 0) %>%
+      select(sim, team, outcome) %>%
+      right_join(teams, by = c("sim", "team")) %>%
+      mutate(exit = ifelse(!is.na(outcome), week_num, exit)) %>%
+      select(-outcome)
+
+    # if super bowl, record winner
+    if (any(playoff_teams$conf == "SB")) {
+      # super bowl winner exit is +1 to SB week
+      teams <- games %>%
+        filter(week == week_num) %>%
+        double_games() %>%
+        filter(outcome == 1) %>%
+        select(sim, team, outcome) %>%
+        right_join(teams, by = c("sim", "team")) %>%
+        mutate(exit = ifelse(!is.na(outcome), week_num + 1, exit)) %>%
+        select(-outcome)
+    }
+
+    # filter to winners or byes
+    playoff_teams <- games %>%
+      filter(week == week_num) %>%
+      double_games() %>%
+      right_join(playoff_teams, by = c("sim", "team")) %>%
+      filter(is.na(result) | result > 0) %>%
+      select(sim, conf, seed, team) %>%
+      arrange(sim, conf, seed)
+
+    # update number of teams
+    num_teams <- playoff_teams %>%
+      group_by(sim, conf) %>%
+      summarize(count = n()) %>%
+      pull(count) %>%
+      max()
+
+    # if at one team per conf, loop once more for the super bowl
+    if (num_teams == 1 && !any(playoff_teams$conf == "SB")) {
+      playoff_teams <- playoff_teams %>%
+        mutate(conf = "SB", seed = rep(1:2, n() / 2))
+      num_teams <- 2
+    }
+  } # end playoff loop
+
+  #### DRAFT ORDER ####
+
+  teams <- standings_and_h2h %>%
+    compute_draft_order(
+      games = games,
+      h2h = h2h_df,
+      tiebreaker_depth = tiebreaker_depth,
+      .debug = .debug
+    )
+
+  p(sprintf("finished sim round %g", sim_round))
+
+  list("teams" = teams, "games" = games)
+}
---
+++ R/simulate_nfl.R
@@ -0,0 +1,199 @@
+#' Simulate an NFL Season
+#'
+#' @inheritParams compute_conference_seeds
+#' @param nfl_season Season to simulate
+#' @param estimate_games A function to estimate the games to simulate. Uses the
+#'   schedule as argument.
+#' @param simulate_games A function to simulate the results of the games to
+#'   simulate. Uses the schedule and the week as argument.
+#' @param if_ended_today Either \code{TRUE} or \code{FALSE}. ...
+#' @param fresh_season Either \code{TRUE} or \code{FALSE}. ...
+#' @param fresh_playoffs Either \code{TRUE} or \code{FALSE}. ...
+#' @param simulations Equals the number of times the given NFL season shall be simulated
+#' @param sims_per_round The number of \code{simulations} can be split into
+#'   multiple rounds and be processed parallel. This parameter controls the number
+#'   of simulations per round.
+#' @description This function simulates a given NFL season multiple times using custom functions
+#'   to estimate and simulate game results and computes the outcome of the given
+#'   season including playoffs and draft order.
+#'   It is possible to run the function in parallel processes by calling the
+#'   appropriate \link[future]{plan}.
+#'   Progress updates can be activated by calling \link[progressr]{handlers}
+#'   before the start of the simulations.
+#' @export
+#' @examples
+#' \donttest{
+#'   library(nflseedR)
+#'   library(dplyr)
+#'
+#'   # round out (away from zero)
+#'   round_out <- function(x) {
+#'     x[x < 0] <- floor(x[x < 0])
+#'     x[x > 0] <- ceiling(x[x > 0])
+#'     return(x)
+#'   }
+#'
+#'   # function to estimate games
+#'   my_estimate <- function(g) {
+#'     # replace with your own function
+#'     # g = games data
+#'
+#'     # define
+#'     # estimate = is the median spread expected (positive = home team favored)
+#'     # wp = is the probability of the team winning the game
+#'
+#'     # this example estimates at PK/0 and 50%
+#'
+#'     g <- g %>%
+#'       dplyr::mutate(estimate = 0, wp = 0.5)
+#'     return(g)
+#'   }
+#'
+#'   # function to simulate games
+#'   my_simulate <- function(g, w) {
+#'     # replace with your own function
+#'     # only simulate games through week w
+#'     # only simulate games with is.na(result)
+#'
+#'     # define
+#'     # result = how many points home team won by
+#'     # can add additional columns as well (e.g. Elo)
+#'
+#'     g <- g %>%
+#'       dplyr::mutate(result = ifelse(is.na(result) & week <= w,
+#'         round_out(rnorm(n(), estimate, 14)),
+#'         result
+#'       ))
+#'     return(g)
+#'   }
+#'
+#'   # Activate progress updates
+#'   progressr::handlers(global = TRUE)
+#'
+#'   # Parallel processing can be activated via the following line
+#'   # future::plan("multisession")
+#'
+#'   # Simulate season 4 times in 2 rounds
+#'   sim <- nflseedR::simulate_nfl(
+#'     2020,
+#'     estimate_games = my_estimate,
+#'     simulate_games = my_simulate,
+#'     fresh_season = TRUE,
+#'     simulations = 4,
+#'     sims_per_round = 2
+#'   )
+#' }
+simulate_nfl <- function(nfl_season,
+                         estimate_games,
+                         simulate_games,
+                         playoff_seeds = ifelse(nfl_season >= 2020, 7, 6),
+                         if_ended_today = FALSE,
+                         fresh_season = FALSE,
+                         fresh_playoffs = FALSE,
+                         tiebreaker_depth = 3,
+                         simulations = 10,
+                         sims_per_round = simulations,
+                         .debug = FALSE) {
+
+
+  #### LOAD DATA ####
+
+  # load games data
+  report("Loading games data")
+  schedule <- readRDS(url("https://github.com/leesharpe/nfldata/blob/master/data/games.rds?raw=true")) %>%
+    filter(season == nfl_season) %>%
+    select(game_type, week, away_team, home_team, result)
+
+  #### PREPROCESSING ####
+
+  # week to examine leverage
+  if (isTRUE(fresh_season) || isTRUE(if_ended_today)) {
+    leverage <- FALSE # override, leverage makes no sense here
+  } else leverage <- TRUE
+
+  if (isTRUE(leverage)) {
+    interest_week <- schedule %>%
+      filter(!is.na(result)) %>%
+      pull(week) %>%
+      min()
+  }
+
+  # if simulating fresh season, clear out all results and playoff games
+  if (isTRUE(fresh_season)) {
+    schedule <- schedule %>%
+      filter(game_type == "REG") %>%
+      mutate(result = as.numeric(NA))
+  }
+
+  # if simulating fresh playoffs, clear out playoff games
+  if (isTRUE(fresh_playoffs)) {
+    schedule <- schedule %>%
+      filter(game_type == "REG")
+  }
+
+  # if ended today just needs one simulation
+  if (isTRUE(if_ended_today)) {
+    schedule <- schedule %>%
+      filter(!is.na(result))
+    simulations <- 1
+  }
+
+  # weeks to sim
+  weeks_to_sim <- schedule %>%
+    filter(game_type == "REG") %>%
+    filter(is.na(result)) %>%
+    pull(week) %>%
+    unique() %>%
+    sort()
+
+  #### SET UP SIMULATIONS ####
+  sim_rounds <- ceiling(simulations / sims_per_round)
+
+  report(glue("Beginning simulation of {simulations} seasons in {sim_rounds} rounds"))
+
+  p <- progressr::progressor(along = seq_len(sim_rounds))
+
+  suppressMessages({
+    all <- furrr::future_map(
+      .x = seq_len(sim_rounds),
+      .f = simulate_round,
+      sim_rounds = sim_rounds,
+      sims_per_round = sims_per_round,
+      schedule = schedule,
+      simulations = simulations,
+      weeks_to_sim = weeks_to_sim,
+      estimate_games = estimate_games,
+      simulate_games = simulate_games,
+      tiebreaker_depth = tiebreaker_depth,
+      .debug = .debug,
+      playoff_seeds = playoff_seeds,
+      p = p,
+      .options = furrr::furrr_options(seed = TRUE)
+    )
+  })
+
+  report("Combining simulation data")
+
+  all_teams <- furrr::future_map_dfr(all, ~ .x$teams)
+  all_games <- furrr::future_map_dfr(all, ~ .x$games)
+
+  # aggregated data
+  report("Aggregating across simulations")
+  overall <- all_teams %>%
+    group_by(conf, division, team) %>%
+    summarize(
+      wins = mean(wins),
+      playoff = mean(!is.na(seed)),
+      div1 = mean(div_rank == 1),
+      seed1 = mean(!is.na(seed) & seed == 1),
+      won_conf = mean(draft_order >= (length(unique(all_teams$team)) - 1)),
+      won_sb = mean(draft_order == length(unique(all_teams$team))),
+      draft1 = mean(draft_order == 1),
+      draft5 = mean(draft_order <= 5)
+    ) %>%
+    ungroup()
+
+  print(overall)
+
+  list("teams" = all_teams, "games" = all_games, "overall" = overall)
+}
