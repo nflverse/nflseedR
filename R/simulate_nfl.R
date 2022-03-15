140fe10aecd309ff17e15db6616ff63301518684
mrcaseb <roemerle@gmail.com>
2022-03-15 11:52:19 +0100

    fix cross-reference check warning

--- R/simulate_nfl.R
+++ R/simulate_nfl.R
@@ -80,7 +80,7 @@
 #'   summary statistics across all simulated seasons, and the siumulation parameters. For a full list,
 #'   please see [the package website](https://nflseedr.com/articles/articles/nflsim.html#simulation-output).
 #' @seealso The examples [on the package website](https://nflseedr.com/articles/articles/nflsim.html)
-#' @seealso The method [summary.nflseedR_simulation()] that creates a pretty html summary table.
+#' @seealso The method [nflseedR::summary.nflseedR_simulation()] that creates a pretty html summary table.
 #' @export
 #' @examples
 #' \donttest{
