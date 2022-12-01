ccd4e843b21edc6e8f19afc615ec55ae744556ae
mrcaseb <roemerle@gmail.com>
2022-12-01 12:24:04 +0100

    report now fully cli compatible including themes and pluralization

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 
 # progress report using rlang to avoid usethis dependency
-report <- function(msg) {
-  cli::cli_alert_info("{format(Sys.time(), '%H:%M:%S')} | {msg}")
+report <- function(msg, .cli_fct = cli::cli_alert_info) {
+  .cli_fct(c(format(Sys.time(), '%H:%M:%S'), " | ", msg))
 }
 
 sim_info <- function(msg) {
