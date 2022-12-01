019f7500a5cbf50ca71bbc71690d4b6c7dd5086b
mrcaseb <roemerle@gmail.com>
2022-12-01 09:53:58 +0100

    fix messaging

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,7 @@
 
 # progress report using rlang to avoid usethis dependency
 report <- function(msg) {
-  rlang::inform(
-    paste0(
-      crayon::red(cli::symbol$bullet),
-      " ",
-      Sys.time(),
-      ": ",
-      msg
-    )
-  )
+  cli::cli_alert_info("{format(Sys.time(), '%H:%M:%S')} | {msg}")
 }
 
 sim_info <- function(msg) {
