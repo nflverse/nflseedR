9ec7705c2bd30f52ba7141c70d268972eb39696f
mrcaseb <mrcaseb@gmail.com>
2024-10-08 14:35:25 +0200

    add new internal constants

--- data-raw/internal_constants.R
+++ data-raw/internal_constants.R
@@ -3,10 +3,19 @@ TIEBREAKERS_NONE        <- 1
 TIEBREAKERS_NO_COMMON   <- 2
 TIEBREAKERS_THROUGH_SOS <- 3
 
+div_vec <- nflseedR::divisions$division |>
+  rlang::set_names(nflseedR::divisions$team)
+
+conf_vec <- nflseedR::divisions$conf |>
+  rlang::set_names(nflseedR::divisions$team)
+
 usethis::use_data(
   TIEBREAKERS_NONE,
   TIEBREAKERS_NO_COMMON,
   TIEBREAKERS_THROUGH_SOS,
+  div_vec,
+  conf_vec,
   internal = TRUE,
   overwrite = TRUE
 )
+
