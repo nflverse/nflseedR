d296fead3c5ee41b4746be2225c8e1cb9ce912e9
Sebastian Carl <mrcaseb@gmail.com>
2024-10-30 14:39:22 +0100

    update data and redocument

--- data-raw/sim_examples.R
+++ data-raw/sim_examples.R
@@ -5,16 +5,20 @@ sims_teams_example <- sims_teams_example[rep(seq_len(nrow(sims_teams_example)), 
   select(sim, dplyr::everything())
 
 sims_games_example <- nflreadr::load_schedules(2022) |>
-  dplyr::filter(game_type == "REG")
+  # dplyr::filter(game_type == "REG") |>
+  dplyr::select(
+    "season", "game_type", "week", "away_team", "home_team",
+    "away_rest", "home_rest", "location", "result", "game_id"
+  )
 reset_ids <- sims_games_example |>
   dplyr::slice_sample(n = 30) |>
   dplyr::pull(game_id)
 sims_games_example <- sims_games_example |>
   dplyr::mutate(
-    result = data.table::fifelse(game_id %in% reset_ids, NA_integer_, result)
-  )
-sims_games_example <- nflseedR:::sims_validate_games(sims_games_example) |>
-  tibble::as_tibble()
+    result = data.table::fifelse(game_id %in% reset_ids, NA_integer_, result),
+    result = data.table::fifelse(game_type == "SB", NA_integer_, result)
+  ) |>
+  dplyr::select(-"game_id")
 
 usethis::use_data(sims_teams_example, sims_games_example, overwrite = TRUE)
 
