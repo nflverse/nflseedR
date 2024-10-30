cfbea9f4c53513f52f7674e18b8a165a34bca3b8
Sebastian Carl <mrcaseb@gmail.com>
2024-10-30 14:34:13 +0100

    improve performance of default function by dropping merge statements

--- R/simulations_utils.R
+++ R/simulations_utils.R
@@ -139,13 +139,10 @@ sims_compute_playoff_dummy <- function(num_byes){
   playoff_games
 }
 
-default_compute_results <- function(teams, games, week_num, ...) {
-  # cli::cli_progress_step(
-  #   "Compute week {.val #{week_num}}"
-  # )
-  # teams = teams data
-  # games = games data
-  #
+
+#' @export
+#' @noRd
+nflseedR_compute_results <- function(teams, games, week_num, ...) {
   # this example estimates at PK/0 and 50%
   # estimate = is the median spread expected (positive = home team favored)
   # wp = is the probability of the team winning the game
@@ -156,8 +153,9 @@ default_compute_results <- function(teams, games, week_num, ...) {
 
   # round out (away from zero)
   round_out <- function(x) {
-    x[!is.na(x) & x < 0] <- floor(x[!is.na(x) & x < 0])
-    x[!is.na(x) & x > 0] <- ceiling(x[!is.na(x) & x > 0])
+    x <- x[!is.na(x)]
+    x[x < 0] <- floor(  x[x < 0])
+    x[x > 0] <- ceiling(x[x > 0])
     as.integer(x)
   }
 
@@ -165,49 +163,51 @@ default_compute_results <- function(teams, games, week_num, ...) {
   setDT(teams, key = c("sim", "team"))
 
   # get elo if not in teams data already
-  if (!("elo" %chin% colnames(teams))) {
+  # elo is expected to be a named vector of elo ratings where
+  # names are NFL team abbreviations
+  if (!"elo" %chin% colnames(teams)) {
+    # Query arguments in dots and see if elo is in there
+    # if not, set it to random values
     args <- list(...)
     if ("elo" %chin% names(args)) {
       # pull from custom arguments
-      ratings <- setDT(args$elo, key = "team")
-      teams <- merge(teams, ratings[,list(team, elo)])
+      ratings <- args$elo
+      teams[, elo := ratings[team]]
     } else {
-      # start everyone at a random default elo
-      ratings <- data.table(
-        team = unique(teams$team),
-        elo = rnorm(length(unique(teams$team)), 1500, 150),
-        key = "team"
+      # if custom elo is missing in dots, start everyone at a random elo
+      ratings <- setNames(
+        rnorm(length(unique(teams$team)), 1500, 150),
+        unique(teams$team)
       )
-      teams <- merge(teams, ratings)
+      teams[, elo := ratings[team]]
     }
   }
 
-  # merge elo values to home and away teams
-  games <- merge(x = games, y = teams[,list(sim, team, away_elo = elo)],
-                 by.x = c("sim", "away_team"),
-                 by.y = c("sim", "team"),
-                 sort = FALSE,
-                 all.x = TRUE)
-  games <- merge(x = games, y = teams[,list(sim, team, home_elo = elo)],
-                 by.x = c("sim", "home_team"),
-                 by.y = c("sim", "team"),
-                 sort = FALSE,
-                 all.x = TRUE)
+  # At this point, there is a column named "elo" in the teams data
+  # We use that column to create a elo lookup vector that we use to
+  # add those elo ratings to home and away teams in the games data
+  # Names of that vector are built off sim and team to make sure
+  # we don't mix elo values of one team across simulations
+  ratings <- teams[, setNames(elo, paste(sim, team, sep = "-"))]
+
+  # fill elo values of home and away teams
+  games[week_num, away_elo := ratings[paste(sim, away_team, sep = "-")], on = "week"]
+  games[week_num, home_elo := ratings[paste(sim, home_team, sep = "-")], on = "week"]
 
   # create elo diff
-  games[, elo_diff := home_elo - away_elo + (home_rest - away_rest) / 7 * 25]
+  games[week_num, elo_diff := home_elo - away_elo + (home_rest - away_rest) / 7 * 25, on = "week"]
   # adjust elo diff for location = HOME
-  games["Home", elo_diff := elo_diff + 20, on = "location"]
+  games[list(week_num, "Home"), elo_diff := elo_diff + 20, on = c("week", "location")]
   # adjust elo_diff for game type = REG
-  games["REG", elo_diff := elo_diff * 1.2, on = "game_type"]
+  games[list(week_num, "REG"), elo_diff := elo_diff * 1.2, on = c("week", "game_type")]
   # create wp and estimate
-  games[, `:=`(wp = 1 / (10^(-elo_diff / 400) + 1),
-               estimate = elo_diff / 25)]
+  games[week_num, `:=`(wp = 1 / (10^(-elo_diff / 400) + 1),
+                       estimate = elo_diff / 25), on = "week"]
   # adjust result in current week
   games[week_num == week & is.na(result),
         result := round_out(rnorm(.N, estimate, 13))]
   # compute elo shift
-  games[, `:=`(
+  games[week_num, `:=`(
     outcome = fcase(
       is.na(result), NA_real_,
       result > 0, 1,
@@ -220,153 +220,28 @@ default_compute_results <- function(teams, games, week_num, ...) {
       result < 0, -elo_diff * 0.001 + 2.2,
       default = 1.0
     )
-  )]
-  games[, elo_mult := log(pmax(abs(result), 1) + 1.0) * 2.2 / elo_input]
-  games[, elo_shift := 20 * elo_mult * (outcome - wp)]
+  ), on = "week"]
+  games[week_num, elo_mult := log(pmax(abs(result), 1) + 1.0) * 2.2 / elo_input, on = "week"]
+  games[week_num, elo_shift := 20 * elo_mult * (outcome - wp), on = "week"]
 
-  # drop irrelevant columns
+  # Build elo_shift vector based on results if home and away teams
+  elo_change_away <- games[week_num, setNames(-elo_shift, paste(sim, away_team, sep = "-")), on = "week"]
+  elo_change_home <- games[week_num, setNames(elo_shift,  paste(sim, home_team, sep = "-")), on = "week"]
+  elo_change <- c(elo_change_away, elo_change_home)
+
+  # drop helper columns
   drop_cols <- c("away_elo", "home_elo", "elo_diff", "wp", "estimate",
-                 "outcome", "elo_input", "elo_mult")
+                 "outcome", "elo_input", "elo_mult", "elo_shift")
   games[, (drop_cols) := NULL]
 
-  # apply away team elo shift
-  away_teams <- games[list(week_num),
-                      list(sim, team = away_team, elo_shift = -elo_shift),
-                      on = "week"]
-  teams <- merge(teams, away_teams, by = c("sim", "team"), all = TRUE)
-  teams[!is.na(elo_shift), elo := elo + elo_shift]
+  # apply elo shift in teams data to transport new elo values to next week
+  teams[, elo_shift := elo_change[paste(sim, team, sep = "-")]]
+  # teams that didn't play that week are missing in elo_change. Their shift
+  # value will be NA. We set it to 0 to be able to add the shift for all teams
+  teams[, elo_shift := fifelse(is.na(elo_shift), 0, elo_shift)]
+  teams[, elo := elo + elo_shift]
+  # remove the shift variable for this round
   teams[, elo_shift := NULL]
 
-  # apply home team elo shift
-  home_teams <- games[list(week_num),
-                      list(sim, team = home_team, elo_shift),
-                      on = "week"]
-  teams <- merge(teams, home_teams, by = c("sim", "team"), all = TRUE)
-  teams[!is.na(elo_shift), elo := elo + elo_shift]
-  teams[, elo_shift := NULL]
-
-  games[, elo_shift := NULL]
   list("teams" = teams, "games" = games)
-}
-
-validate_userfunction <- function(compute_results,
-                                  ...,
-                                  games = nflseedR::sims_games_example,
-                                  teams = nflseedR::sims_teams_example){
-
-  if (!is.function(compute_results)) {
-    cli::cli_abort("The {.arg compute_results} argument must be a function!")
-  }
-
-  games <- data.table::as.data.table(games)
-
-  weeks_to_simulate <- games[game_type == "REG" & is.na(result), sort(unique(week))]
-
-  for (week_num in weeks_to_simulate) {
-    # recall old data for comparison
-    old_teams <- data.table::copy(teams)
-    old_games <- data.table::copy(games)[,.old_result := result]
-
-    return_value <- compute_results(
-      teams = teams,
-      games = games,
-      week_num = week_num,
-      ...
-    )
-
-    # did we get the right data back?
-    # currently, we will catch a maximum of 9 problems. Allocate the vector
-    problems <- vector("character", length = 9L)
-    i <- 0
-    if (typeof(return_value) != "list") {
-      i <- i + 1
-      problems[i] <- "the returned value was not a list"
-    } else {
-      if (!("teams" %in% names(return_value))) {
-        i <- i + 1
-        problems[i] <- "`teams` was not in the returned list"
-      } else {
-        teams <- return_value$teams
-        if (nrow(teams) != nrow(old_teams)) {
-          i <- i + 1
-          problems[i] <- paste(
-            "`teams` changed from", nrow(old_teams), "to",
-            nrow(teams), "rows",
-            collapse = " "
-          )
-        }
-        if ( any( !colnames(old_teams) %in% colnames(teams) ) ){
-          i <- i + 1
-          removed_names <- colnames(old_teams)[!colnames(old_teams) %in% colnames(teams)]
-          problems[i] <- paste(
-            "`teams` column(s) `", removed_names, "` removed",
-            collapse = ", "
-          )
-        }
-      }
-      if (!("games" %in% names(return_value))) {
-        i <- i + 1
-        problems[i] <- "`games` was not in the returned list"
-      } else {
-        games <- return_value$games
-        if (nrow(games) != nrow(old_games)) {
-          i <- i + 1
-          problems[i] <- paste(
-            "`games` changed from", nrow(old_games), "to",
-            nrow(games), "rows",
-            collapse = " "
-          )
-        }
-        if ( any( !colnames(old_games) %in% colnames(games) ) ){
-          i <- i + 1
-          removed_names <- colnames(old_games)[!colnames(old_games) %in% colnames(games)]
-          problems[i] <- paste(
-            "`games` column(s) `", removed_names, "` removed",
-            collapse = ", "
-          )
-        }
-      }
-    }
-  }
-
-  # report data structure problems
-  problems <- problems[problems != ""]
-  if (length(problems)) {
-    cli::cli_abort(c(
-      "During Week {.val {week_num}}, the {.code compute_results} function \\
-       produced the following issues:",
-      sapply(problems, function(p) c("x" = p), USE.NAMES = FALSE)
-    ))
-  }
-
-  # identify improper results values
-  problems <- old_games %>%
-    inner_join(games, by = intersect(colnames(old_games), colnames(games))) %>%
-    mutate(problem = case_when(
-      week == week_num & is.na(result) ~
-        "a result from the current week is missing",
-      week != week_num & !is.na(.old_result) & is.na(result) ~
-        "a known result outside the current week was blanked out",
-      week != week_num & is.na(.old_result) & !is.na(result) ~
-        "a result outside the current week was entered",
-      week != week_num & .old_result != result ~
-        "a known result outside the current week was updated",
-      !is.na(.old_result) & is.na(result) ~
-        "a known result was blanked out",
-      !is.na(result) & result == 0 & game_type != "REG" ~
-        "a playoff game resulted in a tie (had result == 0)",
-      TRUE ~ NA_character_
-    )) %>%
-    filter(!is.na(problem)) %>%
-    pull(problem) %>%
-    unique()
-
-  # report result value problems
-  if (problems != "") {
-    cli::cli_abort(
-      "During Week {week_num}, your {.code process_games} function had the \\
-        following issues: {problems}. Make sure you only change results \\
-        when {.code week == week_num} & {.code is.na(result)}"
-    )
-  }
 }
